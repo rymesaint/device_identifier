3467ec324e8f7b98353866b326ae6d29323577bd
Ridwan Fauzan <ridwanfauzan@kunci.co.id>
2025-10-13 14:55:26 +0700

    refactor: update project dependencies and migrate to Flutter 3.18
    - Update Gradle and Kotlin versions for Android
    - Migrate to AndroidX and enable Jetifier
    - Update Flutter SDK and Dart SDK constraints
    - Upgrade cupertino_icons dependency
    - Refactor MainActivity to use FlutterActivity
    - Update Android manifest and styles for Flutter embedding v2
    - Remove deprecated Flutter.podspec

--- android/src/main/kotlin/com/hespinola/device_identifier/DeviceIdentifierPlugin.kt
+++ android/src/main/kotlin/com/hespinola/device_identifier/DeviceIdentifierPlugin.kt
@@ -4,25 +4,41 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.provider.Settings.Secure
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class DeviceIdentifierPlugin(activity: Activity): MethodCallHandler {
+class DeviceIdentifierPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+  private lateinit var channel: MethodChannel
   private var activity: Activity? = null
 
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "device_identifier")
-      channel.setMethodCallHandler(DeviceIdentifierPlugin(registrar.activity()))
-    }
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "device_identifier")
+    channel.setMethodCallHandler(this)
   }
 
-  init {
-      this.activity = activity
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
   }
 
   @SuppressLint("NewApi", "HardwareIds")
